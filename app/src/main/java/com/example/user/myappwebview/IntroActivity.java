743eb8d38a780ba2fb8d3ede8369a04f8eb9247f
kgbank <kgbank@gmail.com>
2016-06-25 14:51:45 +0900

    WebView 이미지 삽입

--- app/src/main/java/com/example/user/myappwebview/IntroActivity.java
+++ app/src/main/java/com/example/user/myappwebview/IntroActivity.java
@@ -1,13 +1,30 @@
 package com.example.user.myappwebview;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends Activity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+        findViewById(R.id.btWebView).setOnClickListener(this);
+        findViewById(R.id.btImageView).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btWebView:
+                startActivity(new Intent(this, MainActivity.class));
+                break;
+            case R.id.btImageView:
+                startActivity(new Intent(this, ImageViewActivity.class));
+                break;
+        }
     }
 }
